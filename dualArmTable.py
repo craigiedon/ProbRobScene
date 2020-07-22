968f18d610130b6450c942c35dc18a86fe44b4f0
Craig Innes <craiginnes@gmail.com>
2020-07-22 18:09:55 +0100

    Added trays with cubes, plus robots now pick up cube and move it

--- dualArmTable.py
+++ dualArmTable.py
@@ -8,7 +8,47 @@ from pyrep.backend import sim
 from multiprocessing import Process
 import numpy as np
 
-PROCESSES = 1
+
+def grasp(pr, gripper, close: bool) -> None:
+    if close:
+        pos = 0.05
+    else:
+        pos = 0.9
+
+    actuated = False
+    i = 0
+    while not actuated:
+        actuated = gripper.actuate(pos, 0.1)
+        print("Actuation steps: {}".format(i))
+        pr.step()
+        i += 1
+    pr.step()
+
+    return
+
+
+def move_above_object(pr, agent, target_obj, z_offset=0.05):
+    pos = target_obj.get_position()
+    pos[2] = pos[2] + z_offset
+
+    path = agent.get_path(position=pos, euler=[-np.pi, 0.0, np.pi / 2.0], ignore_collisions=True)  # , euler=orient)
+    # path.visualize()
+
+    done = False
+    while not done:
+        done = path.step()
+        pr.step()
+    pr.step()
+    return
+
+
+def move_to_pos(pr, agent, pos):
+    path = agent.get_path(position=pos, euler=[-np.pi, 0, np.pi/2])
+    done = False
+    while not done:
+        done = path.step()
+        pr.step()
+    pr.step()
 
 
 # def run():
@@ -31,6 +71,28 @@ script_call("table_height_callback@Table", table_script, floats=[0.8])
 table_bounds = table.get_bounding_box()
 table_top_z = table_bounds[-1]
 
+# Trays Creation
+
+in_tray = pr.import_model("assets/Tray.ttm")
+in_tray.set_position([0.1, -0.4, table_top_z], relative_to=table)
+
+out_tray = pr.import_model("assets/Tray.ttm")
+out_tray.set_position([0.1, 0.4, table_top_z], relative_to=table)
+
+c1_color = [0.5, 0.0, 0.0]
+c2_color = [0.0, 0.5, 0.0]
+c3_color = [0.0, 0.0, 0.5]
+cube_dims = [0.05, 0.05, 0.05]
+
+c1 = Shape.create(type=PrimitiveShape.CUBOID, color=c1_color, size=cube_dims, visible_edges=True, mass=0.01)
+c1.set_position([0, 0, 0.01 + cube_dims[-1] * 0.5], relative_to=in_tray)
+
+# c2 = Shape.create(type=PrimitiveShape.CUBOID, color=c2_color, size=cube_dims)
+# c2.set_position([0.10, 0.0, 0.01 + cube_dims[-1] * 0.5], relative_to=in_tray)
+
+# c3 = Shape.create(type=PrimitiveShape.CUBOID, color=c3_color, size=cube_dims)
+# c3.set_position([-0.10, 0.0, 0.01 + cube_dims[-1] * 0.5], relative_to=in_tray)
+
 pr.step_ui()
 
 panda_1 = Panda(0)
@@ -39,32 +101,8 @@ gripper_1 = PandaGripper(0)
 panda_2 = Panda(1)
 gripper_2 = PandaGripper(1)
 
-panda_1.set_position([-0.5, -0.6, table_top_z], relative_to=table)
-
-print("joint positions: ", panda_1.get_joint_positions())
-
-panda_2.set_position([-0.5, 0.6, table_top_z], relative_to=table)
-
-
-c1_color = [150.0, 0.0, 0.0]
-c2_color = [0.0, 150.0, 0.0]
-c3_color = [0.0, 0.0, 150.0]
-cube_dims = [0.05, 0.05, 0.05]
-
-c1 = Shape.create(type=PrimitiveShape.CUBOID, 
-                  color=c1_color,
-                  size=cube_dims)
-c1.set_position([0, 0, table_top_z + cube_dims[-1] * 0.5], relative_to=table)
-
-c2 = Shape.create(type=PrimitiveShape.CUBOID, 
-                  color=c2_color,
-                  size=cube_dims)
-c2.set_position([0.0, 0.2, table_top_z + cube_dims[-1] * 0.5], relative_to=table)
-
-c3 = Shape.create(type=PrimitiveShape.CUBOID, 
-                  color=c3_color,
-                  size=cube_dims)
-c3.set_position([0.0, 0.4, table_top_z + cube_dims[-1] * 0.5], relative_to=table)
+panda_1.set_position([-0.5, -0.4, table_top_z + 0.07], relative_to=table) # Panda-base is (inexplicably!) slightly above bottom of panda
+panda_2.set_position([-0.5, 0.4, table_top_z + 0.07], relative_to=table)
 
 
 print("Robot joint angles: ", panda_1.get_joint_positions())
@@ -72,15 +110,51 @@ print("Robot joint velocities: ", panda_1.get_joint_velocities())
 
 pr.start()
 
-panda_1.set_joint_positions([-0.8, 0.7, 3.305009599330333e-08, -0.8726646304130554, -1.1409618139168742e-07, 1.2217304706573486, 0.7853981256484985])
+# panda_1.set_joint_positions([-0.8, 0.7, 3.305009599330333e-08, -0.8726646304130554, -1.1409618139168742e-07, 1.2217304706573486, 0.7853981256484985])
+
+# move_above_object(panda_1, c1)
+# move_above_object(panda_1, c2)
+
+move_above_object(pr, panda_1, c1, z_offset=0.00)
+grasp(pr, gripper_1, True)
+gripper_1.grasp(c1)
+
+
+lift_pos = panda_1.get_tip().get_position()
+lift_pos[2] += 0.1
+move_to_pos(pr, panda_1, lift_pos)
+
+lift_pos[1] += 0.45
+
+move_to_pos(pr, panda_1, lift_pos)
+
+gripper_1.release()
+grasp(pr, gripper_1, False)
+
+lift_pos[1] -= 0.45
+
+move_to_pos(pr, panda_1, lift_pos)
+
+move_above_object(pr, panda_2, c1, z_offset=0.0)
+grasp(pr, gripper_2, True)
+gripper_2.grasp(c1)
+
+move_above_object(pr, panda_2, out_tray, z_offset=0.1)
+gripper_2.release()
+grasp(pr, gripper_2, False)
+
 
 for i in range(1000):
     if (i % 100) == 0:
         print("step", i)
     pr.step()
 
-pr.stop()
 
+# print("Grasp 3 happened")
+# grasp(pr, gripper_2, False)
+# print("Grasp 4 happened")
+
+pr.stop()
 
 
 pr.shutdown()
